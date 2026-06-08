aab147babb6e67dd97a62d7f93685e197abf43d4
DarryllPj77 <pajaganasdarryll2004@gmail.com>
2026-06-08 22:02:23 +0800

    For Grade Calculator

---
+++ Grade Calculator/MathCalculator/BasicArithmetic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Grade_Calculator.MathCalculator
+{
+    internal class BasicArithmetic
+    {
+        public static double AddNumbers(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double SubNumbers(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double MultiNumbers(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double DivNumbers(double a, double b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Cannot divide by zero. Divisor must not be zero.");
+            }
+            return a / b;
+        }
+    }
+}
---
+++ Grade Calculator/Program.cs
@@ -0,0 +1,55 @@
+﻿using Grade_Calculator.MathCalculator;
+using System.Threading.Channels;
+namespace Grade_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator");
+            int choice = 0;
+            object result = 0;
+
+            do
+            {
+                Console.Write("Enter a number: ");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter a number: ");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter a number to what arithmetic will be used: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        result = BasicArithmetic.AddNumbers(num1, num2);
+                        Console.WriteLine($"The result is: {result}");
+                        break;
+                    case 2:
+                        result = BasicArithmetic.SubNumbers(num1, num2);
+                        Console.WriteLine($"The result is: {result}");
+                        break;
+                    case 3:
+                        result = BasicArithmetic.MultiNumbers(num1, num2);
+                        Console.WriteLine($"The result is: {result}");
+                        break;
+                    case 4:
+                        result = BasicArithmetic.DivNumbers(num1, num2);
+                        Console.WriteLine($"The result is: {result}");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input!");
+                        break;
+                }
+            } while (choice >= 4);
+
+            //object result1 = Calculator.AddNumbers(num1, num2);
+            //object result2 = Calculator.Subtraction(num1, num2);
+
+            //Console.WriteLine($"The result is: {result1}");
+            //Console.WriteLine($"The result is: {result2}");
+
+        }
+    }
+}
