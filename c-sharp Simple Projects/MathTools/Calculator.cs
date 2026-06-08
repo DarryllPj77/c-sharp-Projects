22975734356aecb14048f50f4a42af5f771b97b0
DarryllPj77 <pajaganasdarryll2004@gmail.com>
2026-06-08 22:04:21 +0800

    Remove bin and obj folders, add .gitignore

---
+++ c-sharp Simple Projects/MathTools/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace c_sharp_Simple_Projects.MathTools
+{
+    internal class Calculator
+    {
+        public static int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ c-sharp Simple Projects/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Transactions;
+
+using c_sharp_Simple_Projects.MathTools;
+
+namespace c_sharp_Simple_Projects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator");
+            int result = Calculator.AddNumbers(5, 10);
+
+            Console.WriteLine($"The result is: {result}");
+        }
+    }
+}
